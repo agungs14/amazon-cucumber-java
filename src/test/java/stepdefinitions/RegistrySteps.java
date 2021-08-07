d96169a2b6d96215405ebd21a722555e3e3064ff
agung setyawan <agungsetyawan906@gmail.com>
2021-08-07 17:58:29 +0700

    amazon cucumber java first commit

---
+++ src/test/java/stepdefinitions/RegistrySteps.java
@@ -0,0 +1,46 @@
+package stepdefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class RegistrySteps {
+
+    @Given("^i open the website amazon\\.com for registry and gifting$")
+    public void i_open_the_website_amazon_com_for_registry_and_gifting() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Given("^make sure i am at page registry and gifting$")
+    public void make_sure_i_am_at_page_registry_and_gifting() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^i find registry with name john and gift list type: birthday gift$")
+    public void i_find_registry_with_name_john_and_gift_list_type_birthday_gift() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^make sure the list result for john$")
+    public void make_sure_the_list_result_for_john() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^i search criteria by range month from january (\\d+) to april (\\d+)$")
+    public void i_search_criteria_by_range_month_from_january_to_april(int arg1, int arg2) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^make sure the search result is an accordance with the specific time range$")
+    public void make_sure_the_search_result_is_an_accordance_with_the_specific_time_range() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+}
---
+++ src/test/resources/features/registry.feature
@@ -0,0 +1,11 @@
+Feature: Registry and gifting
+  As a user amazon, i want to registry and gifting
+  So that i can use this site: amazon.com
+
+  Scenario: registry and gifting
+    Given i open the website amazon.com for registry and gifting
+    And make sure i am at page registry and gifting
+    When i find registry with name john and gift list type: birthday gift
+    Then make sure the list result for john
+    When i search criteria by range month from january 2021 to april 2021
+    Then make sure the search result is an accordance with the specific time range
